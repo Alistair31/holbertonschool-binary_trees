e28be0e43b5e6f77379602806d4c21a30fa4c3a1
Alistair31 <gabikill@hotmail.fr>
2025-12-09 15:26:00 +0100

    fix: betty styles correction, space deleted

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,15 +5,14 @@
  * @func: Pointer to a function to call for each node. The value in the node
  *        must be passed as a parameter to this function
  */
- void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
- {
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
 
 	if (tree == NULL || func == NULL)
 		return;
 
-	
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
-	
- }
+
+}
