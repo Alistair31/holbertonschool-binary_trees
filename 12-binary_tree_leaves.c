ed16736e01ba89e10886c8398a47db2e1db42db0
Iglcyril <iglcyril@hotmail.fr>
2025-12-09 17:07:03 +0000

    fix: betty synthax

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,7 +12,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return(1);
+		return (1);
 
 	left_leaves = binary_tree_leaves(tree->left);
 	right_leaves = binary_tree_leaves(tree->right);
