89ead93f87075896d9aa588d408838113d42a24a
Iglcyril <iglcyril@hotmail.fr>
2025-12-09 09:15:58 +0000

    fix: betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,7 +9,7 @@
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
-	
+
 	if (parent == NULL)
 		return (NULL);
 
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->left = NULL;
 	new_node->right = parent->right;
 
-	if(parent->right != NULL)
+	if (parent->right != NULL)
 	parent->right->parent = new_node;
 
 		parent->right = new_node;
